9ca08bc5db5a134bfadc2663d2751db0d150c1a9
mash43 <dev@mash43.com>
2026-02-17 12:26:07 +0900

    fix: capitalize manifest name

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,10 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "wxt";
+import { name } from "./package.json";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
 	modules: ["@wxt-dev/i18n/module", "@wxt-dev/module-solid"],
 	manifest: () => ({
+		name: name.charAt(0).toUpperCase() + name.slice(1),
 		permissions: ["storage", "activeTab"],
 		default_locale: "en",
 		author: { email: import.meta.env.WXT_EMAIL },
